3f2c2b0c3e54690a4024946fd58f5dd664ebcdad
David Rivard <55965524+Dave-21@users.noreply.github.com>
2025-11-18 22:13:33 -0500

    public page

--- docs/forecasts/forecast_data.js
+++ docs/forecasts/forecast_data.js
@@ -3,24 +3,24 @@ window.forecastData = {
         "narrative": "Critical build-up: **Lake Superior:** Soo Locks (Forming). **Lake Michigan:** Mackinac (Forming). **Lake Huron:** St Clair River (Forming).",
         "shipping": "Transit caution advised.",
         "title": "Forecast: T0",
-        "image": "/forecasts/forecast_T0.png"
+        "image": "./forecasts/forecast_T0.png"
     },
     "T1": {
         "narrative": "Critical build-up: **Lake Superior:** Duluth (Forming), Soo Locks (Heavy). **Lake Michigan:** Green Bay (Forming), Mackinac (Heavy). **Lake Huron:** St Clair River (Forming). **Lake Erie:** Buffalo (Forming).",
         "shipping": "BLOCKAGE at Soo Locks. BLOCKAGE at Mackinac.",
         "title": "Forecast: T1",
-        "image": "/forecasts/forecast_T1.png"
+        "image": "./forecasts/forecast_T1.png"
     },
     "T2": {
         "narrative": "Critical build-up: **Lake Superior:** Duluth (Forming), Whitefish Bay (Forming), Soo Locks (Heavy). **Lake Michigan:** Green Bay (Forming), Mackinac (Heavy). **Lake Huron:** St Clair River (Forming). **Lake Erie:** Buffalo (Forming).",
         "shipping": "BLOCKAGE at Soo Locks. BLOCKAGE at Mackinac.",
         "title": "Forecast: T2",
-        "image": "/forecasts/forecast_T2.png"
+        "image": "./forecasts/forecast_T2.png"
     },
     "T3": {
         "narrative": "Critical build-up: **Lake Superior:** Duluth (Forming), Whitefish Bay (Forming), Soo Locks (Heavy). **Lake Michigan:** Green Bay (Forming), Mackinac (Heavy). **Lake Huron:** St Clair River (Forming). **Lake Erie:** Buffalo (Forming).",
         "shipping": "BLOCKAGE at Soo Locks. BLOCKAGE at Mackinac.",
         "title": "Forecast: T3",
-        "image": "/forecasts/forecast_T3.png"
+        "image": "./forecasts/forecast_T3.png"
     }
 };
